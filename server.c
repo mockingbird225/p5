9c84e1639062e203a2818b24a7a1159df4155b41
mockingbird225 <anusha136@gmail.com>
2014-04-13 17:08:43 -0500

    Test

--- server.c
+++ server.c
@@ -10,6 +10,59 @@
 // Repeatedly handles HTTP requests sent to this port number.
 // Most of the work is done within routines written in request.c
 //
+pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t consumerCV = PTHREAD_COND_INITIALIZER;
+pthread_cond_t producerCV = PTHREAD_COND_INITIALIZER;
+
+typedef Queue_Req Queue; 
+struct Queue_Req {
+	int q[BUFFER_SIZE];
+	int front;
+	int rear;
+	int count;
+};
+
+void initializeQueue(Queue* q) {
+	q->front = 0;
+	q->rear = BUFFER_SIZE - 1;
+	q->count = 0;
+}
+
+void enqueue(Queue* q, int x) {
+	if(q->count >= BUFFER_SIZE) {
+		printf("Queue overflow\n");
+	} else {
+		q->rear = (q->rear + 1) % BUFFER_SIZE;
+		q->q[q->rear] = x;
+		q->count++;
+	}
+}
+
+int isQueueEmpty(Queue *q) {
+	if(q->count <= 0) {
+		return 1;
+	}
+	return 0;
+}
+
+int isQueueFull(Queue* q) {
+	if(q->count >= BUFFER_SIZE) {
+		return 1;	
+	}
+	return 0;
+}
+
+int dequeue(Queue* q) {
+	int x;
+	if(q->count <= 0) {
+		printf("Queue empty\n");
+	} else {
+		x = q->q[q->front];
+		q->front = (q->front + 1) % BUFFER_SIZE;
+		q->count--;
+	}
+	return x;
+} 
 
 // CS537: TODO make it parse the new arguments too
 int getargs(int *port, int *numThreads, int* numBuffers, char* sAlgo, int argc, char* argv[]) {
@@ -58,12 +111,46 @@ int getargs(int *port, int *numThreads, int* numBuffers, char* sAlgo, int argc, 
 	*port = atoi(argv[1]);
 }*/
 
+void* handleRequest(void* queue) {
+	int req;
+	int qFullFlag = 0;
+	Queue* q = (Queue*)queue;
+	pthread_mutex_lock(&lock);
+	while(isQueueEmpty(&q)) {
+		pthread_cond_wait(&consumerCV, &lock);	
+	}		
+	if(isQueueFull()) {
+		qFullFlag = 1;
+	}
+	req = dequeue(&q);
+	requestHandle(req);
+	Close(req);
+	if(qFullFlag) {
+		pthread_cond_signal(&producerCV);
+	}
+	pthread_mutex_unlock(&lock);
+}
+
+void processConn(int connFd) {
+	int qEmptyFlag = 0;
+	pthread_mutex_lock(&lock);
+	if(isQueueFull()) {
+		pthread_cond_wait(&producerCV, &lock);
+	}	
+	enqueue(connFd);
+	if(qEmptyFlag) {
+		pthread_cond_signal(&consumerCV);
+	}
+	pthread_mutex_unlock(&lock);
+}
 
 int main(int argc, char *argv[])
 {
+	Queue q;
 	int listenfd, connfd, clientlen;
 	int port, numThreads, numBuffers, numReq, temp;
 	char sAlgo[MAX];
+	int i;
 	struct sockaddr_in clientaddr;
 
 	//getargs(&port, argc, argv);
@@ -71,28 +158,34 @@ int main(int argc, char *argv[])
 	if(temp >= 0) {
 		numReq = temp;
 	}
+	pthread_t threads[numThreads];
 	/* ------------Test----------------
 	printf("Port: %d, numThreads : %d, numBuffers : %d, Algo : %s", port, numThreads, numBuffers, sAlgo);
 	*/ 
 	// CS537: TODO create some worker threads using pthread_create ...
 	//
-
-	/*listenfd = Open_listenfd(port);
+	for(i = 0; i < numThreads; i++) {
+		pthread_create(&threads[i], NULL, handleRequest, (void*)&q);
+	}
+	for(i = 0; i < numThreads; i++) {
+		pthread_join(threads[i], NULL);
+	}
+	listenfd = Open_listenfd(port);
 	while (1) {
 		clientlen = sizeof(clientaddr);
 		connfd = Accept(listenfd, (SA *)&clientaddr, (socklen_t *) &clientlen);
-
+		processConn(connfd, &q);
 		// 
 		// CS537: In general, don't handle the request in the main thread.
 		// TODO In multi-threading mode, the main thread needs to
 		// save the relevant info in a buffer and have one of the worker threads
 		// do the work.
 		// 
-		requestHandle(connfd);
+		//requestHandle(connfd);
 
-		Close(connfd);
-	}*/
-
+		//Close(connfd);
+	}
+	return 0;
 }
 
 
