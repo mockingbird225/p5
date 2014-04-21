4ea5785bb6cd7b66a3a3c63ee9144291fec3f87d
mockingbird225 <anusha136@gmail.com>
2014-04-21 18:21:01 -0500

    LastCommit

--- server.c
+++ server.c
@@ -10,65 +10,80 @@
 // Repeatedly handles HTTP requests sent to this port number.
 // Most of the work is done within routines written in request.c
 //
+int numBuffers;
 pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
 pthread_cond_t consumerCV = PTHREAD_COND_INITIALIZER;
 pthread_cond_t producerCV = PTHREAD_COND_INITIALIZER;
 
-typedef Queue_Req Queue; 
+
+typedef struct Queue_Req Queue; 
 struct Queue_Req {
-	int q[BUFFER_SIZE];
+	int *qElts;
+	int bufferSize;
+	int *fileSize;
+	char* algo; // 0-FIFO 1-SFF 2-SFF-BS
+	int numReq; // Not SFF-BS, then 0;
 	int front;
 	int rear;
 	int count;
 };
 
-void initializeQueue(Queue* q) {
-	q->front = 0;
-	q->rear = BUFFER_SIZE - 1;
+void initializeQueue(Queue* q, char* sAlgo, int _numReq) {
+	q->bufferSize = numBuffers;
+	q->qElts = malloc(sizeof(int)*(q->bufferSize));
+	q->front = -1;
+	q->rear = -1;
 	q->count = 0;
+	q->fileSize = malloc(sizeof(int)*(q->bufferSize));
+	q->algo = sAlgo;
+	q->numReq = _numReq;
+}
+
+void printQueue(Queue* q) {
+	int i;
+	printf("BufferSize: %d, Algo: %s, NumReq: %d, Front: %d, Rear: %d, Count: %d\n", q->bufferSize, q->algo, q->numReq, q->front, q->rear, q->count);
+	printf("Elements in queue\n");
+	for(i = 0; i < q->front; i++) {
+		printf("%d ", q->qElts[i]);
+	}	
 }
 
 void enqueue(Queue* q, int x) {
-	if(q->count >= BUFFER_SIZE) {
+	printf("Enqueue here\n");
+	if(q->count >= q->bufferSize) {
 		printf("Queue overflow\n");
 	} else {
-		q->rear = (q->rear + 1) % BUFFER_SIZE;
-		q->q[q->rear] = x;
+		q->rear = q->rear + 1;
+		q->qElts[(q->rear % q->bufferSize)] = x;
 		q->count++;
 	}
 }
 
 int isQueueEmpty(Queue *q) {
-	if(q->count <= 0) {
-		return 1;
-	}
-	return 0;
+	return (q->front == q->rear);
 }
 
 int isQueueFull(Queue* q) {
-	if(q->count >= BUFFER_SIZE) {
-		return 1;	
-	}
-	return 0;
+	return ((q->rear - q->bufferSize) == q->front);
 }
 
 int dequeue(Queue* q) {
 	int x;
-	if(q->count <= 0) {
+	if(isQueueEmpty(q)) {
 		printf("Queue empty\n");
 	} else {
-		x = q->q[q->front];
-		q->front = (q->front + 1) % BUFFER_SIZE;
-		q->count--;
+		q->front++;
+		x = q->qElts[(q->front % q->bufferSize)];
+		return x;
 	}
-	return x;
+	return -1;
 } 
 
 // CS537: TODO make it parse the new arguments too
 int getargs(int *port, int *numThreads, int* numBuffers, char* sAlgo, int argc, char* argv[]) {
 	int numReq = -1;
 	if(argc < 5 || argc > 6) {
-		fprintf(stderr, "Usage : %s [port] [threads] [buffers] [schedalg] [N (for SFF-BS only)]\n");
+		fprintf(stderr, "Usage : %s [port] [threads] [buffers] [schedalg] [N (for SFF-BS only)]\n", argv[0]);
 		exit(1);
 	}
 	*port = atoi(argv[1]);
@@ -90,7 +105,7 @@ int getargs(int *port, int *numThreads, int* numBuffers, char* sAlgo, int argc, 
 		strcpy(sAlgo, argv[4]);
 		if(!strcmp(sAlgo, "SFF-BS")) {
 			if(argc == 5) {
-				fprintf(stderr, "Usage : %s [port] [threads] [buffers] [schedalg] [N (for SFF-BS only)]\n");
+				fprintf(stderr, "Usage : ./server [port] [threads] [buffers] [schedalg] [N (for SFF-BS only)]\n");
 				exit(1);
 			}
 			numReq = atoi(argv[5]);
@@ -111,18 +126,17 @@ int getargs(int *port, int *numThreads, int* numBuffers, char* sAlgo, int argc, 
 	*port = atoi(argv[1]);
 }*/
 
-void* handleRequest(void* queue) {
+void fifo(Queue* q) {
 	int req;
 	int qFullFlag = 0;
-	Queue* q = (Queue*)queue;
 	pthread_mutex_lock(&lock);
-	while(isQueueEmpty(&q)) {
+	while(isQueueEmpty(q)) {
 		pthread_cond_wait(&consumerCV, &lock);	
 	}		
-	if(isQueueFull()) {
+	if(isQueueFull(q)) {
 		qFullFlag = 1;
 	}
-	req = dequeue(&q);
+	req = dequeue(q);
 	requestHandle(req);
 	Close(req);
 	if(qFullFlag) {
@@ -131,13 +145,39 @@ void* handleRequest(void* queue) {
 	pthread_mutex_unlock(&lock);
 }
 
-void processConn(int connFd) {
+void sff(Queue* q) {
+	
+}
+
+void sffbs(Queue* q) {
+	printf("SFF-BS with %d requests\n", q->numReq);
+}
+
+void* handleRequest(void* queue) {
+	Queue* q = (Queue*)queue;
+	if(!strcmp(q->algo, "FIFO")) {
+		// First in first out implementation
+		fifo(q);
+	} else if(!strcmp(q->algo, "SFF")) {
+		//Smallest File First implementation
+		sff(q);
+	} else if(!strcmp(q->algo, "SFF-BS")) {
+		// Smallest File first with bounded starvation implementation 
+		sffbs(q);
+	}
+	return 0;
+}
+
+void processConn(int connFd, Queue* q) {
 	int qEmptyFlag = 0;
 	pthread_mutex_lock(&lock);
-	if(isQueueFull()) {
+	if(isQueueFull(q)) {
 		pthread_cond_wait(&producerCV, &lock);
 	}	
-	enqueue(connFd);
+	if(isQueueEmpty(q)) {
+		qEmptyFlag = 1;
+	}
+	enqueue(q, connFd);
 	if(qEmptyFlag) {
 		pthread_cond_signal(&consumerCV);
 	}
@@ -148,7 +188,8 @@ int main(int argc, char *argv[])
 {
 	Queue q;
 	int listenfd, connfd, clientlen;
-	int port, numThreads, numBuffers, numReq, temp;
+	int port, numThreads, numReq, temp;
+	int rc;
 	char sAlgo[MAX];
 	int i;
 	struct sockaddr_in clientaddr;
@@ -164,17 +205,28 @@ int main(int argc, char *argv[])
 	*/ 
 	// CS537: TODO create some worker threads using pthread_create ...
 	//
+	initializeQueue(&q, sAlgo, numReq);
+
+	
+		
+	
 	for(i = 0; i < numThreads; i++) {
-		pthread_create(&threads[i], NULL, handleRequest, (void*)&q);
+		rc = pthread_create(&threads[i], NULL, handleRequest, (void*)&q);
+		if(rc != 0) {
+			fprintf(stderr, "Thread creation failed\n");
+		}
 	}
-	for(i = 0; i < numThreads; i++) {
+	/*for(i = 0; i < numThreads; i++) {
 		pthread_join(threads[i], NULL);
-	}
+	}*/
 	listenfd = Open_listenfd(port);
 	while (1) {
 		clientlen = sizeof(clientaddr);
 		connfd = Accept(listenfd, (SA *)&clientaddr, (socklen_t *) &clientlen);
 		processConn(connfd, &q);
+		// -------------------Printing the queue-------------
+		printQueue(&q);
+
 		// 
 		// CS537: In general, don't handle the request in the main thread.
 		// TODO In multi-threading mode, the main thread needs to
