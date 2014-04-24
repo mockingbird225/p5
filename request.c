2ec58c08eb5e7b84b691b763632c81acf43301d3
mockingbird225 <anusha136@gmail.com>
2014-04-24 13:51:14 -0500

    Commit

--- request.c
+++ request.c
@@ -255,6 +255,7 @@ void findReqSize(int fd, int* isStatic, int* fileSize, int* modeErr, char* cgiar
 	} else {
 		if (!(S_ISREG((_sbuf).st_mode)) || !(S_IXUSR & (_sbuf).st_mode)) {
 	        	*modeErr = 1;
+		*fileSize = 0;
 		}
 	}
 }
