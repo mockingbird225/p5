2b56301471f249f205ceea97eccfb234dd297ee0
mockingbird225 <anusha136@gmail.com>
2014-04-25 18:39:31 -0500

    Full version 2.6

--- server.c
+++ server.c
@@ -500,7 +500,7 @@ void processConn(int connFd, suseconds_t _statReqArrival) {
 			findReqSize(connFd, &(_isStatic), &(_fileSize),  &modeErr, _cgiargs, _method, _uri, _version, _filename);
 			enqueueSff(connFd, _isStatic, _fileSize, modeErr, _cgiargs, _method, _uri, _version, _filename, _statReqArrival);
 		} else {
-			if(count >= numReq) {
+			if(tempBuffer < 0) {
 				//printf("Greater than epoch\n");	
 				pthread_cond_wait(&epochCV, &lock);
 			}
